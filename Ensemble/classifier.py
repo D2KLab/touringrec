38ce7fdd7255b5d539ae9b7df5e7576edfcd9c5a
Andrea Fiandro <andrea.fiandro@gmail.com>
2019-06-09 18:37:29 +0200

    Terminato classificatore SVM

--- Ensemble/classifier.py
+++ Ensemble/classifier.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import numpy as np
 import sys
+import matplotlib.pyplot as plt
+from mlxtend.plotting import plot_decision_regions
+from sklearn.metrics import classification_report
+from prettytable import PrettyTable
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+
 
 def get_session_stats(df_gt):
     '''
@@ -23,6 +33,48 @@ def calculate_best(rank_mf, rank_rnn):
     else:
         return 1
 
+def splitTrainValidationTest(samples, labels, nTrain, nValidation, nTest):
+    fracTrain = nTrain / 10  # percentage of training data (e.g. 5:2:3 -> 0.5 of train)
+    x_train, x_test, y_train, y_test = train_test_split(samples, labels, test_size=fracTrain, random_state=1)
+    fracValidation = nValidation / (10 - nTrain)  # Take the percentage of validation
+    x_test, x_validation, y_test, y_validation = train_test_split(samples, labels, test_size=fracValidation,
+                                                                  random_state=1)
+    return x_train, x_test, x_validation, y_train, y_test, y_validation
+
+'''
+    This function receive the training set and the test set and will train the chosen model with the C parameters provided
+'''
+def evaluateModel(x_train, y_train, myC, x_test, y_test, kernel_type):
+
+    svc = svm.SVC(kernel=kernel_type, C=myC)
+    svc.fit(x_train, y_train)
+    # Evaluate accuracy score
+    pr = svc.predict(x_test)
+    score = accuracy_score(y_test, pr)
+    print('SVM with ' + kernel_type + " kernel accuracy: " + str(score))
+    return
+
+def testClassifier(kerneltype, bestC, bestGamma, x_train, y_train, x_test, y_test):
+    svc = svm.SVC(kernel=kerneltype, random_state=0, C=bestC, gamma=bestGamma, probability=True)
+    svc.fit(x_train, y_train)
+    #plot_decision_regions(x_train, y_train, clf=svc, legend=2)
+    #plt.show()
+    pr = svc.predict(x_test)
+    score = accuracy_score(y_test, pr)
+    return score, svc
+'''
+'''
+def kFoldParamEvaluation(x_train, y_train, x_test, y_test, c_values, gamma_values):
+    tuned_params = [{'kernel': ['rbf','linear'], 'gamma': gamma_values, 'C': c_values }]
+    clf = GridSearchCV(svm.SVC(), tuned_params, cv=5)
+    clf.fit(x_train, y_train)
+    print("Best gamma and C found: ")
+    print()
+    print(clf.best_params_)
+    y_true, y_predict = y_test, clf.predict(x_test)
+    #print(classification_report(y_true, y_predict))
+    return clf.best_params_
+
 def generate_labels(df):
     df['label'] = df.apply(lambda x: calculate_best(x.rank_mf, x.rank_rnn), axis = 1)
     return df
@@ -42,6 +94,23 @@ print('Remove ties')
 df_merged = df_merged[df_merged['rank_mf'] != df_merged['rank_rnn']]
 print('Remove single click')
 df_merged = df_merged[df_merged['session_length'] != 1]
-df_input_svm = generate_labels(df_merged)
-df_input_svm = df_input_svm[['session_id', 'session_length', 'sparsity', 'label']]
-print(df_input_svm.head())
+df_data_svm = generate_labels(df_merged)
+print(df_data_svm[['session_id', 'session_length', 'sparsity']].head())
+x = df_data_svm[['session_length', 'sparsity']].values
+y = df_data_svm[['label']].values.ravel()
+print('INPUT')
+print(x.shape)
+print('LABEL')
+print(y.shape)
+
+# Find the best parameters of the RBF kernel
+
+myGamma = [0.01, 0.1, 1, 10]
+myC = [0.01, 0.1, 1, 10]
+x_train, x_test, x_validation, y_train, y_test, y_validation = splitTrainValidationTest(x, y, 7, 1, 2)
+best_params = kFoldParamEvaluation(x_train, y_train, x_validation, y_validation, myC, myGamma)
+bestScore, svmModel = testClassifier(best_params['kernel'], best_params["C"], best_params["gamma"], x_train, y_train, x_test, y_test)
+print(bestScore)
+test = [[3,1]]
+label = svmModel.predict_proba(test)
+label
