ff8faad2311bbb02491328a15fff118c36685135
Gogo <crepaldigiorgio@hotmail.it>
2019-06-11 22:41:15 +0200

    Increased net size

--- SplitDataset/split.py
+++ SplitDataset/split.py
@@ -3,6 +3,13 @@ import numpy as np
 import functions as f
 import argparse
 
+def remove_null_clickout(df):
+    """
+    Remove all the occurences where the clickout reference is set to null (Item to predict)
+    """
+    df = df.drop(df[(df['action_type'] == "clickout item") & (df['reference'].isnull())].index)
+    return df
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--percentage', action="store", type=int, help="Define the percentage of the dataset that you want to split")
@@ -28,3 +35,8 @@ f.split_group_csv(df_train, session_train, session_test, 'session_id', percentag
 df_test = pd.read_csv('gt_' + percentage_s + '.csv')
 df_cleaned = f.clean_dataset(df_test)
 df_cleaned.to_csv('test_' + percentage_s + '.csv', index= False)
+
+#creating encode set by removing null clickouts from test set
+df_test_no_null = remove_null_clickout(df_cleaned)
+df_encode = pd.concat([df_train, df_test_no_null])
+df_encode.to_csv('encode_' + percentage_s + '.csv')
