2a0eca6d2eaf8455a59c92823a0ed65feaa70592
gogo <giorgio.crepaldi93@gmail.com>
2019-06-24 11:28:07 +0200

    remove single actions optimized

--- SetupScript/RNN/setup.py
+++ SetupScript/RNN/setup.py
@@ -136,23 +136,24 @@ if param.ismeta:
 #corpus = dsm.get_corpus(df_encode)
 
 df_train_inner = pd.read_csv('./train.csv')
-df_train_inner = dsm.remove_single_actions(df_train_inner)
+df_train_inner = dsm.remove_single_clickout_actions(df_train_inner)
 #df_train_inner = dsm.remove_single_actions_opt(df_train_inner)
 df_train_inner =  dsm.remove_nonitem_actions(df_train_inner)
 #df_train_inner = dsm.reference_to_str(df_train_inner)
 
 df_test_inner = pd.read_csv('./test.csv')
-df_test_inner = dsm.remove_single_actions(df_test_inner)
+df_test_inner = dsm.remove_single_clickout_actions(df_test_inner)
 #df_test_inner = dsm.remove_single_actions_opt(df_test_inner)
 df_test_inner = dsm.remove_nonitem_actions(df_test_inner)
 #df_test_inner = dsm.reference_to_str(df_test_inner)
 
 df_gt_inner = pd.read_csv('./gt.csv')
+df_gt_inner = dsm.remove_single_clickout_actions(df_gt_inner)
 
 #df_test_inner, df_gt_inner = dsm.remove_test_single_actions(df_test_inner, df_gt_inner)
 
 df_test_dev = pd.read_csv('./test_off.csv')
-df_test_dev = dsm.remove_single_actions(df_test_dev)
+df_test_dev = dsm.remove_single_clickout_actions(df_test_dev)
 #df_test_dev = dsm.remove_single_actions_opt(df_test_dev)
 df_test_dev = dsm.remove_nonitem_actions(df_test_dev)
 #df_test_dev = dsm.reference_to_str(df_test_dev)
