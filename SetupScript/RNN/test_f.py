54940d704300fba516c78dd7945a91a6c0d0f7bf
gogo <giorgio.crepaldi93@gmail.com>
2019-07-05 16:23:42 +0200

    removed accuracy computation

--- SetupScript/RNN/test_f.py
+++ SetupScript/RNN/test_f.py
@@ -400,10 +400,10 @@ def test_accuracy_optimized_classification(model, df_test, df_gt, session_dict, 
       df_sub.to_csv('./' + subname + '.csv')
 
   # Computing mrr only if test set is not the one without gt
-  if dev:
-    mrr = 0
-  else:
-    mrr = score_submissions_no_csv(df_sub, df_gt, get_reciprocal_ranks)
+  #if dev:
+  #  mrr = 0
+  #else:
+  #  mrr = score_submissions_no_csv(df_sub, df_gt, get_reciprocal_ranks)
 
-  return mrr
+  return 0
   
